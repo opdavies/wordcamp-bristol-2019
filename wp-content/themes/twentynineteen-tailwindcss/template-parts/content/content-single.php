4f8773d4b563cae44a1a8e0b848622c0f4f4b50d
Oliver Davies <oliver@oliverdavies.uk>
2019-03-28 10:06:11 +0000

    Update spacing on a single post

---
+++ wp-content/themes/twentynineteen-tailwindcss/template-parts/content/content-single.php
@@ -0,0 +1,42 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( ! twentynineteen_can_show_post_thumbnail() ) : ?>
+	<header class="entry-header">
+		<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+	</header>
+	<?php endif; ?>
+
+	<div class="mt-12">
+		<?php
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentynineteen' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
+
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'twentynineteen' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer mt-12 mb-20">
+		<?php twentynineteen_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+	<?php if ( ! is_singular( 'attachment' ) ) : ?>
+	<?php get_template_part( 'template-parts/post/author', 'bio' ); ?>
+	<?php endif; ?>
+
+</article><!-- #post-${ID} -->
