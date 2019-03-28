ceb21b007e6501c26479f2e308c10e13d37c5509
Oliver Davies <oliver@oliverdavies.uk>
2019-03-28 08:52:08 +0000

    Use fluent API

--- wp-content/themes/twentynineteen-tailwindcss/webpack.mix.js
+++ wp-content/themes/twentynineteen-tailwindcss/webpack.mix.js
@@ -1,15 +1,15 @@
-const mix = require('laravel-mix')
+let mix = require('laravel-mix')
 
-mix.postCss('src/css/style.css', '', [
-  require('tailwindcss')('./tailwind.config.js'),
-  require('postcss-nested')(),
-])
-
-mix.browserSync({
-  proxy: 'wordcamp2019.docksal',
-  notify: false,
-  files: [
-    '**/*.php',
-    'src/**/*.css'
-  ],
-})
+mix.disableNotifications()
+  .postCss('src/css/style.css', '', [
+    require('tailwindcss')('./tailwind.config.js'),
+    require('postcss-nested')(),
+  ])
+  .browserSync({
+    proxy: 'wordcamp2019.docksal',
+    notify: false,
+    files: [
+      '**/*.php',
+      'src/css/**/*.css'
+    ],
+  })
