619403d57f7481fd1876dcc82d93e51a78cd950e
Oliver Davies <oliver@oliverdavies.uk>
2019-03-28 08:52:17 +0000

    Decrease top margin for widgets

--- wp-content/themes/twentynineteen-tailwindcss/functions.php
+++ wp-content/themes/twentynineteen-tailwindcss/functions.php
@@ -9,7 +9,7 @@ add_action('widgets_init', function () {
       'id' => 'tw-sidebar',
       'before_title' => '<h2 class="text-4xl font-bold text-gray-800 mt-6 mb-4 tracking-tight">',
       'after_title' => '</h2>',
-      'before_widget' => '<div class="mt-16 w-1/2 px-6">',
+      'before_widget' => '<div class="mt-10 w-1/2 px-6">',
       'after_widget' => '</div>',
     )
   );
