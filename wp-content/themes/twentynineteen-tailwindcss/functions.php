b8445625e4cfe075d3376443bc4b91fb34d03688
Oliver Davies <oliver@oliverdavies.uk>
2019-03-28 08:44:50 +0000

    Inline function, update title styles

--- wp-content/themes/twentynineteen-tailwindcss/functions.php
+++ wp-content/themes/twentynineteen-tailwindcss/functions.php
@@ -2,16 +2,15 @@
 
 wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-function tw_widgets() {
+add_action('widgets_init', function () {
   register_sidebar(
     array(
       'name' => 'Sidebar',
       'id' => 'tw-sidebar',
-      'before_title' => '<h2 class="text-4xl font-bold text-gray-800">',
+      'before_title' => '<h2 class="text-4xl font-bold text-gray-800 mt-6 mb-4 tracking-tight">',
       'after_title' => '</h2>',
       'before_widget' => '<div class="mt-16 w-1/2 px-6">',
       'after_widget' => '</div>',
     )
   );
-}
-add_action('widgets_init', 'tw_widgets');
+});
