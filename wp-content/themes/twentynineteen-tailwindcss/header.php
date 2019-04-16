e70b21e517c8aa041d2c3e0951f6def631568222
Oliver Davies <oliver@oliverdavies.uk>
2019-04-16 20:22:52 +0100

    Adjust text colour and container width

--- wp-content/themes/twentynineteen-tailwindcss/header.php
+++ wp-content/themes/twentynineteen-tailwindcss/header.php
@@ -5,8 +5,8 @@
     <title><?php echo wp_get_document_title(); ?></title>
     <?php wp_head(); ?>
   </head>
-  <body class="font-sans bg-white text-gray-800 leading-normal antialiased">
-    <div class="max-w-4xl mx-auto px-4">
+  <body class="font-sans bg-white text-gray-900 leading-normal antialiased">
+    <div class="max-w-6xl mx-auto px-4">
       <header class="mt-16 mb-24">
         <p class="text-2xl">
           <?php bloginfo('slogan'); ?>
