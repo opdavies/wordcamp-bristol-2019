80f9b51fe01f3b9985609d2ec92b3396cf4fa7b5
Oliver Davies <oliver@oliverdavies.uk>
2019-06-07 22:18:43 +0100

    Split up Mix config

--- wp-content/themes/twentynineteen-tailwindcss/webpack.mix.js
+++ wp-content/themes/twentynineteen-tailwindcss/webpack.mix.js
@@ -1,22 +1,19 @@
 let mix = require('laravel-mix')
 
 mix.disableNotifications()
-  .postCss('resources/css/app.css', 'style.css', [
-    require('postcss-import')(),
-    require('tailwindcss')(),
-    require('postcss-nested')(),
-  ])
-  .browserSync({
-    proxy: 'wordcamp2019.docksal',
-    notify: false,
-    open: false,
-    files: [
-      '**/*.php',
-      {
-        match: 'style.css',
-        fn: function (event, file) {
-          this.reload()
-        }
-      }
-    ]
-  })
+
+mix.postCss('resources/css/app.css', 'style.css', [
+  require('postcss-import')(),
+  require('tailwindcss')(),
+  require('postcss-nested')()
+])
+
+mix.browserSync({
+  proxy: 'wordcamp2019.docksal',
+  notify: false,
+  open: false,
+  files: ['**/*.php', {
+    match: 'style.css',
+    fn: function (event, file) { this.reload() }
+  }]
+})
